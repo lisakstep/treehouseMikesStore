c33624dcd8ba638f70347fbb7069bd9e363ec2be
Lisa Stephens <lisakstep@gmail.com>
2015-01-04 12:35:28 -0700

    initial commit. Store is working but needs polishing. Finished up to the end of Integrating Validation Errors in the TeamTreehouse Advanced PHP lesson.

---
+++ info.php
@@ -0,0 +1,12 @@
+<?php 
+$pageTitle = "Company Information";
+$section = "info";
+include ('inc/header.php'); ?>
+
+<div class="section page">
+
+  <h1>Information about Mike's Shirts</h1>
+
+</div>
+
+<?php include('inc/footer.php'); ?>
---
+++ test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Shirts 4 Mike</title>
+</head>
+<body>
+  <h1>Shirts 4 Mike</h1>
+  <p>&copy;<?php echo date('Y'); ?> Shirts 4 Mike</p>
+  <p>Today is <?php echo date("l, F jS, Y"); ?>.</p>
+</body>
+</html>
+
+
+
